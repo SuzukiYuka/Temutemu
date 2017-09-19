ea4eda46df153c91418e5b7020169ef07e5b7f43
Yuka Suzuki <suzukiyuka1218@gmail.com>
2017-09-19 15:15:46 +0900

    first commit

---
+++ Assets/Scripts/countBall.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class countBall : MonoBehaviour {
+    public Texture[] textures;
+
+    public GameObject particle;
+    public int ballID;
+    public int countID;
+    public static bool connect;
+    public static int countStartID;
+    public GameObject startObj;
+    public static bool particleState = false;
+
+	// Use this for initialization
+	void Start () {
+        startObj = GameObject.Find("BallManager");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        int randomCount = Random.Range(0, 2);
+        if (transform.position.y >= 10) {
+            if (randomCount == 0) {
+                this.tag = "yellow";
+            } else if (randomCount == 1) {
+                this.tag = "blue";
+            }
+
+            GetComponent<Renderer>().material.mainTexture = textures[randomCount];
+            countID = randomCount;
+            gameObject.transform.parent = null;
+		}
+	}
+
+    private void OnMouseDown() {
+
+        particleState = false;
+        countStartID = countID;
+        startObj.transform.position = new Vector3(0, 0, 0);
+        gameObject.transform.parent = startObj.gameObject.transform;
+        connect = true;
+        particle.GetComponent<ParticleSystem>().Play();
+    }
+}
---
+++ Assets/Scripts/startBall.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class startBall : MonoBehaviour {
+    bool startFlg;
+    public GameObject clone;
+	// Use this for initialization
+	void Start () {
+        startFlg = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (startFlg) {
+            for (int i = 0; i < 100; i++) {
+                Instantiate(clone.gameObject, new Vector3(Random.Range(-2f, 2f), Random.Range(30f, 50f), 0), Quaternion.identity);
+                clone.gameObject.GetComponent<countBall>().ballID = i;
+            }
+            startFlg = false;
+        }
+	}
+}
